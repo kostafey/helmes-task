800360b1a6f979eab5681c698395f9298d7e9bf3
kostafey@gmail.com <kostafey@gmail.com>
2022-03-25 01:50:17 +0300

    Add edit user own data during the session

--- app/com/helmes/UserDAO.java
+++ app/com/helmes/UserDAO.java
@@ -4,13 +4,17 @@ import org.hibernate.HibernateException;
 import org.hibernate.Transaction;
 import org.hibernate.Session;
 
+import javax.persistence.TypedQuery;
+import java.util.List;
+import java.util.Optional;
+
 public class UserDAO {
 
-    public static void add(User user) {
+    public static void saveOrUpdate(User user) {
         Session session = HibernateUtil.getSession();
         try {
             Transaction tx = session.beginTransaction();
-            session.save(user);
+            session.saveOrUpdate(user);
             tx.commit();
         } catch (HibernateException e) {
             e.printStackTrace();
@@ -24,6 +28,24 @@ public class UserDAO {
         try {
             user = (User) HibernateUtil.getSession().get(
                     User.class, id);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            HibernateUtil.closeSession();
+        }
+        return user;
+    }
+
+    public static Optional<User> getByName(String name) {
+        Optional<User> user = Optional.empty();
+        try {
+            TypedQuery<User> q = HibernateUtil.getSession().createQuery(
+                    "SELECT u FROM User u WHERE u.name = :name",
+                User.class);
+            List<User> result = q.setParameter("name", name).getResultList();
+            if (result.size() > 0) {
+                user = Optional.of(result.get(0));
+            }
         } catch (HibernateException e) {
             e.printStackTrace();
         } finally {
--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -10,21 +10,12 @@ import play.data.Form;
 import play.mvc.*;
 
 import javax.inject.Inject;
+import java.util.Optional;
 
-/**
- * This controller contains an action to handle HTTP requests
- * to the application's home page.
- */
 public class HomeController extends Controller {
     @Inject
     play.data.FormFactory formFactory;
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
     public Result index() {
         return ok(views.html.index.render());
     }
@@ -33,20 +24,54 @@ public class HomeController extends Controller {
         return ok(CategoryDAO.getCategoriesAsJson()).as("application/json");
     }
 
+    private Result addNewUser(
+            UserForm userForm,
+            Http.Request request) {
+        User user = new User(
+                userForm.getName(),
+                CategoryDAO.get(userForm.getCategoryId()),
+                userForm.getAgreeToTerms());
+        UserDAO.saveOrUpdate(user);
+        return ok().addingToSession(request, "userName", userForm.getName());
+    }
+
+    private Result updateExistingUser(
+            UserForm userForm,
+            User user,
+            Http.Request request) {
+        user.setName(userForm.getName());
+        user.setCategory(CategoryDAO.get(userForm.getCategoryId()));
+        user.setAgreeToTerms(userForm.getAgreeToTerms());
+        UserDAO.saveOrUpdate(user);
+        return ok(user.toJson()).as("application/json")
+                .addingToSession(request, "userName", userForm.getName());
+    }
+
     public Result saveUserData(Http.Request request) {
         UserForm userForm = formFactory.form(UserForm.class)
-            .bindFromRequest(request).get();
-        User user = new User(
-            userForm.getName(),
-            CategoryDAO.get(userForm.getCategoryId()),
-            userForm.getAgreeToTerms());
-        UserDAO.add(user);
-        return ok();
+                .bindFromRequest(request).get();
+        return request
+                .session()
+                .get("userName")
+                .map(userName -> {
+                    Optional<User> user = UserDAO.getByName(userName);
+                    return user
+                            .map(u -> updateExistingUser(userForm, u, request))
+                            .orElseGet(() -> addNewUser(userForm, request));
+                })
+                .orElseGet(() -> addNewUser(userForm, request));
     }
 
     public Result getUserData(Http.Request request) {
-        User user = UserDAO.get(1);
-        UserDAO.add(user);
-        return ok(user.toJson()).as("application/json");
-    }    
+        return request
+                .session()
+                .get("userName")
+                .map(userName -> {
+                    Optional<User> user = UserDAO.getByName(userName);
+                    return user
+                            .map(u -> ok(u.toJson()).as("application/json"))
+                            .orElseGet(() -> ok());
+                })
+                .orElseGet(() -> ok());
+    }
 }
